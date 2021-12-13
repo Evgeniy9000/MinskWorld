03ef0e2af6fb40edb7215e8c1805470060d437d0
Evgeniy9000 <disabled16@mai.ru>
2021-12-13 11:03:11 +0300

    js

--- js/main.js
+++ js/main.js
@@ -3,11 +3,20 @@ $(document).ready(function () {
     let floorPath = $(".home-image path")
     let counterUp = $(".counter-up")
     let counterDown = $(".counter-down")
+    let modal = $(".modal")
+    let modalCloseButton = $(".modal-close-button")
+    let viewFlatsButton = $(".view-flats")
+
+
     floorPath.on("mouseover", function () {
         floorPath.removeClass("current-floor")
         currentFloor = $(this).attr("data-floor");
         $(".counter").text(currentFloor)
     })
+
+    floorPath.on("click", toggleModal);
+    modalCloseButton.on("click", toggleModal);
+    viewFlatsButton.on("click", toggleModal)
 
     counterUp.on("click", function () {
         if (currentFloor < 18) {
@@ -28,6 +37,10 @@ $(document).ready(function () {
             $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor")
         }
     })
+
+    function toggleModal() {
+        modal.toggleClass("is-open");
+    }
 
 });
 
